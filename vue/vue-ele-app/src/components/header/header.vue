e48a7ea3f85cd6de1278cadea092ed7ef248ff05
739226050@qq.com <739226050@qq.com>
2019-11-02 16:38:45 +0800

    'ele'

---
+++ vue/vue-ele-app/src/components/header/header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="header">
+    <div class="content-wrappe">
+      <div class="avatar">
+        <img src="./brand@2x.png" alt="">
+      </div>
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">粥品香仿</span>
+        </div>
+        <div class="discription">蜂鸟传送 30分钟送达</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
