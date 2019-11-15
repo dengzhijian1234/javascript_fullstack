10b95151779e070b3f70371cadf27a4c5ed88a99
739226050@qq.com <739226050@qq.com>
2019-11-15 16:58:30 +0800

    vue-v

---
+++ vue/qunar-webapp/src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+// js 懒加载
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: () => import('@/page/home/Home')
+    },
+    {
+      path: '/city',
+      name: 'City',
+      component: () => import('@/page/city/City'),
+      children: [
+        {
+          path: '/city/in',
+          name: 'CityIn',
+          component: () => import('@/page/city/CityIn')
+        },
+        {
+          path: '/city/out',
+          name: 'CityOut',
+          component: () => import('@/page/city/CityOut')
+        }
+      ]
+    }
+  ]
+})
